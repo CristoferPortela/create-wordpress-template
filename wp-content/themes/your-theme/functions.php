c9b58c2a8ac009bfe66c9cafa61c64f729f9ac5a
CristoferPortela <cristoferportela@gmail.com>
2020-06-22 09:25:36 -0300

    fix: php tag and scripts name changed

--- wp-content/themes/your-theme/functions.php
+++ wp-content/themes/your-theme/functions.php
@@ -1,10 +1,11 @@
+<?php
 function load_assets() {
   $dev = true;
 
   $path = $dev? "https://localhost:8080" : get_template_directory_uri().'/assets/build';
 
-  wp_enqueue_script( 'ong_woo_js', "$path/index.min.js", array(), '1.0.0' , true );
-  wp_enqueue_style( 'ong_woo_css', "$path/style.min.css", array(), '1.0.0', false );
+  wp_enqueue_script( 'theme_js', "$path/index.min.js", array(), '1.0.0' , true );
+  wp_enqueue_style( 'theme_css', "$path/style.min.css", array(), '1.0.0', false );
 
 }
 
